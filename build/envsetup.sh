4ce82f70b790bd0a3b5e292048960669e13d7767
Sankar <34082840+sankarapspk@users.noreply.github.com>
2018-12-29 07:23:39 -0800

    Update envsetup.sh

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -1,30 +1,6 @@
-function __print_aosp_functions_help() {
-cat <<EOF
-Additional AOSP functions:
-- gerrit:          Adds a remote for AEX Gerrit
-EOF
-}
-
 function repopick() {
     T=$(gettop)
     $T/vendor/aosp/build/tools/repopick.py $@
-}
-
-function gerrit()
-{
-    if [ ! -d ".git" ]; then
-        echo -e "Please run this inside a git directory";
-    else
-        if [[ ! -z $(git config --get remote.gerrit.url) ]]; then
-            git remote rm gerrit;
-        fi
-        [[ -z "${GERRIT_USER}" ]] && export GERRIT_USER=$(git config --get gerrit.aospextended.com.username);
-        if [[ -z "${GERRIT_USER}" ]]; then
-            git remote add gerrit $(git remote -v | grep AospExtended | awk '{print $2}' | uniq | sed -e "s|https://github.com/AospExtended|ssh://gerrit.aospextended.com:29418/AospExtended|");
-        else
-            git remote add gerrit $(git remote -v | grep AospExtended | awk '{print $2}' | uniq | sed -e "s|https://github.com/AospExtended|ssh://${GERRIT_USER}@gerrit.aospextended.com:29418/AospExtended|");
-        fi
-    fi
 }
 
 function fixup_common_out_dir() {
