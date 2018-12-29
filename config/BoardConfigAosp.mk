27c69b6cf115d25607d314f0bddddf78da5a78c4
Sankar <34082840+sankarapspk@users.noreply.github.com>
2018-12-29 08:01:27 -0800

    Update BoardConfigAosp.mk

--- config/BoardConfigAosp.mk
+++ config/BoardConfigAosp.mk
@@ -1,7 +1,7 @@
-include vendor/aosp/config/BoardConfigKernel.mk
+include vendor/future/config/BoardConfigKernel.mk
 
 ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
-include vendor/aosp/config/BoardConfigQcom.mk
+include vendor/future/config/BoardConfigQcom.mk
 endif
 
-include vendor/aosp/config/BoardConfigSoong.mk
+include vendor/future/config/BoardConfigSoong.mk
