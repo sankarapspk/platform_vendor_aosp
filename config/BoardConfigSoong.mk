9ea5efa60e2ad00f5f6ae5afe7dbec76a80ae5b3
Sankar <34082840+sankarapspk@users.noreply.github.com>
2018-12-29 07:46:23 -0800

    Update BoardConfigSoong.mk

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -8,7 +8,7 @@ EXPORT_TO_SOONG := \
 
 # Setup SOONG_CONFIG_* vars to export the vars listed above.
 # Documentation here:
-# https://github.com/AospExtended/platform_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
+# https://github.com/FutureExtended/platform_build_soong/commit/8328367c44085b948c003116c0ed74a047237a69
 
 SOONG_CONFIG_NAMESPACES += aospVarsPlugin
 
