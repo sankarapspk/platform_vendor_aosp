dc76c33a91ab588af23703c73ed98343b53c026a
Sankar <34082840+sankarapspk@users.noreply.github.com>
2018-12-29 08:05:08 -0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-for device in $(python vendor/aosp/tools/get_official_devices.py)
+for device in $(python vendor/future/tools/get_official_devices.py)
 do
 for var in eng user userdebug; do
 add_lunch_combo aosp_$device-$var
